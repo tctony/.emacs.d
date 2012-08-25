04c65bf0376ae5d342ff2832cf38a307d66b894d
Tony Tang <tangchang21@gmail.com>
2012-08-26 00:40:51 +0800

    add a function to do google search and bind to f6

--- init-utils.el
+++ init-utils.el
@@ -3,5 +3,18 @@
   (dolist (pattern patterns)
     (add-to-list 'auto-mode-alist (cons pattern mode))))
 
+(defun google-search ()
+  "Look up the word under cursor or the text selection (if exists) on Google using a browser."
+  (interactive)
+  (let (word url)
+    (setq word
+          (if (region-active-p)
+              (buffer-substring-no-properties (region-beginning) (region-end))
+            (thing-at-point 'symbol)))
+    (setq word (url-hexify-string word))
+    (setq url (concat "http://www.google.com/search?q=" word))
+    (browse-url url)
+    ))
+(global-set-key (kbd "<f6>") 'google-search)
 
 (provide 'init-utils)
