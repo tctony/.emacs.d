85ca9fe06b31dfb2f795f475427303ecac46ac29
Tony <tangchang@kokozu.net>
2012-09-05 18:48:03 +0800

    default-tab-width 4

--- init-basic.el
+++ init-basic.el
@@ -7,7 +7,7 @@
 (remove-hook 'text-mode-hook 'turn-on-flyspell)
 (remove-hook 'text-mode-hook 'turn-on-auto-fill)
 
-(setq tab-width 4)
+(setq default-tab-width 4)
 (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80))
 (setq indent-tabs-mode nil)
 
